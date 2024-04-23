b602440c29a2dc2b25bc5b1d7bd24daa14dec9ae
Luiz Gustavo Dias Pulz <162385594+LgZika@users.noreply.github.com>
2024-04-23 22:59:55 +0000

    update

--- exercicioWhile03.cpp
+++ exercicioWhile03.cpp
@@ -5,27 +5,25 @@
 using namespace std;
 
 int main(){
-    long int numero, i = 0, n1 = 1, n2 = 1, newFib, coluna = 2;
+    long int numero, i = 0, n1 = 1, n2 = 1, newFib = 0, coluna = 0;
 
     cout << "Entre com um numero: ";
     cin >> numero;
-    cout.width(15);
-    cout << n1; 
-    cout.width(15);
-    cout << n2;
     
     while(i <= numero){
-        newFib = n1 + n2;                   // soma os dois nuemros anteriores
+        if(i <= 1){
+            newFib = 1;     
+        }else{
+            newFib = n1 + n2;                   // soma os dois numeros anteriores
+            n1 = n2;                            // n1 recebe n2
+            n2 = newFib;                        // n2 recebe o ultimo numero exibido na sequencia
+        }
         cout.width(15);
         cout.left;
-        cout << newFib << "";              // exibe o novo numero fibonacci
-
-        coluna++;                           // variavel das colunas é incrementada (para formatação)
-        if(coluna % 6 == 0){cout << endl;}  // verifica se a coluna é divisivel por 6, se sim, da um enter para nova linha
-
-        n1 = n2;                            // n1 recebe n2
-        n2 = newFib;                        // n2 recebe o ultimo numero exibido na sequencia
+        cout << newFib << "";               // exibe o novo numero fibonacci
         i++;                                // variavel de incremento
+        coluna++;                           // variavel das colunas é incrementada (para formatação)
+        if(coluna % 6 == 0){cout << endl;}  // verifica se a coluna é divisivel por 6, se sim, da um enter para nova linha 
     }
     cout << endl;
     return false;
