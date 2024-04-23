a43720c8627561ec577ca0d4dc510f9074af1f26
Luiz Gustavo Dias Pulz <162385594+LgZika@users.noreply.github.com>
2024-04-23 23:31:05 +0000

    update

---
+++ exercicioFor02Teste.cpp
@@ -0,0 +1,28 @@
+// Escreva um programa que verifica se um número inserido pelo usuário é primo ou não.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numero, cont = 0;
+
+    cout << "Entre com um numero: ";
+    cin >> numero;
+
+    for(int i = 1; i <= numero; i++){
+        cont = 0;
+        for(int j = 1; j <= i; j++){
+            if(i % j == 0){ 
+                cont++;
+            }
+        }
+        if(cont == 2){
+            cout << i << " primo" << endl;
+        }else{
+            cout << i << endl;
+        }
+    }
+   
+    return 0;
+}
